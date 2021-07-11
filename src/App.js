0abf680d7ad6b65b17a682c867b0760cba1cb46a
Alexis Liu <potatojiejie@Alexiss-MacBook-Pro-2.local>
2021-07-11 22:07:24 +0800

    git pull changes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import LandingPage from 'components/landing-page';
 import { UserForm } from 'components/user-form/UserForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import FoodSelectionPage from "./components/food-selection";
-import LandingPage from "./components/landing-page";
-import { UserForm } from "./components/user-form/UserForm";
 
 function App() {
   return (
