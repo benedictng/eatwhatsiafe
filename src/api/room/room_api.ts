5e5ff13eccf8a277564a5ec00007288c425947e6
quekyufei <quekyufei@hotmail.com>
2021-08-09 17:35:29 +0800

    added get food list and mock to room API

--- src/api/room/room_api.ts
+++ src/api/room/room_api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { IRoomAPI, CreateRoomPayload, CreateRoomResponse, RoomStatusPayload, RoomStatusResponse, SubmitVotePayload, SubmitVoteResponse } from "./room_api_interface"
+import { IRoomAPI, CreateRoomPayload, CreateRoomResponse, RoomStatusPayload, RoomStatusResponse, SubmitVotePayload, SubmitVoteResponse, FoodListResponse, FoodListPayload } from "./room_api_interface"
 
 const createRoom = (payload: CreateRoomPayload): Promise<CreateRoomResponse> => {
     const url = 'some url to be separated into a config / env file'
@@ -16,10 +16,16 @@ const submitVote = (payload: SubmitVotePayload): Promise<SubmitVoteResponse> => 
     return axios.post(url, payload)
 }
 
+const getFoodList = (payload: FoodListPayload): Promise<FoodListResponse> => {
+    const url = 'some url to be separated into a config / env file'
+    return axios.post(url, payload)
+}
+
 const RoomAPIImpl : IRoomAPI = {
     createRoom: createRoom,
     getRoomStatus: getRoomStatus,
-    submitVote: submitVote
+    submitVote: submitVote,
+    getFoodList: getFoodList
 }
 
 export default RoomAPIImpl
