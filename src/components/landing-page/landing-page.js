52cec1669882c82c880f84a9d0d6da95473cd5aa
quekyufei <quekyufei@hotmail.com>
2021-08-20 22:00:32 +0800

    change create-enter-choice component to use single text input

--- src/components/landing-page/landing-page.js
+++ src/components/landing-page/landing-page.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import CreateOrEnterRoom from 'components/landing-page/create-enter-choice'
 import GetHostName from 'components/landing-page/get-host-name'
 import GetJoinerName from 'components/landing-page/get-joiner-name'
+import RoomAPI from 'api/room';
 import './landing-page.css'
 
 const LandingPage = () => {
@@ -18,6 +19,27 @@ const LandingPage = () => {
         }
         else {
             // TODO make call for room information NOTE - this call should be done in room. no calls her.
+            RoomAPI.getRoomStatus({room_code: roomCode}).then(res => {
+                switch(res.data.status) {
+                    case 1: // active
+                        setUsersDoneVoting(res.data.voted_users)
+                        setStep(1)
+                        break
+                    case 2: // closed
+
+                        break
+                    default:
+                        // some error
+                        console.log(`Unexpected room status code ${res.data.status}.`)
+                        setStep(4)
+                        setTimeout(() => setStep(0), 1500)
+                        break
+                }
+            })
+            // active: -> get name
+            // closed: -> direct to results
+            // deleted: -> deleted page
+
             setUsersDoneVoting(['bing', 'ben', 'boon'])
             setStep(1)
         }
@@ -36,6 +58,8 @@ const LandingPage = () => {
                 return <GetJoinerName existingUsers={usersDoneVoting} />
             case 2:
                 return <GetHostName createRoom={createRoom}/>
+            case 4:
+                return <p>Error ocurred, refreshing page..</p>
             default:
                 return null
         }
