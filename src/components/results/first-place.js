33ec620c7028741b0f7882a8c683a9e312e103f9
Alexis Liu <potatojiejie@Alexiss-MacBook-Pro-2.local>
2021-08-22 22:43:12 +0800

    just committing so i can merge

--- src/components/results/first-place.js
+++ src/components/results/first-place.js
@@ -23,6 +23,8 @@ const FirstPlace = ({ first_result }) => {
         <h2>{first_result.food.name}</h2>
         <h3>{first_result.food.address}</h3>
         <p>Who voted: {first_result.voted_users}</p>
+        <br></br>
+        <button>Share</button>
 
         {/* {first_results.food.voted_users.map((voters, id) => (
           <h3>{voters}</h3>
