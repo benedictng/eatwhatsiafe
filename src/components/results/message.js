e52bf92713c00b6b08904734217695843b5b0e5c
Alexis Liu <potatojiejie@Alexiss-MacBook-Pro-2.local>
2021-08-23 22:10:29 +0800

    Added copy url function in results page

--- src/components/results/message.js
+++ src/components/results/message.js
@@ -1,6 +1,17 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 const message = ({ is_conclusive }) => {
+  function copy() {
+    const el = document.createElement("input");
+    el.value = window.location.href;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  }
+  console.log(window.location.href);
   return (
     <div>
       <p className="h0">
@@ -13,6 +24,14 @@ const message = ({ is_conclusive }) => {
           this for you.
         </p>
       )}
+      <Container>
+        <Col>
+          <p>{window.location.href}</p>
+        </Col>
+        <Col>
+          <button onClick={copy}>Copy URL</button>
+        </Col>
+      </Container>
     </div>
   );
 };
