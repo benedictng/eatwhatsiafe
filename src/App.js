c9258072d0724fa9bd5aa65075c780d0e907d5e1
quekyufei <quekyufei@hotmail.com>
2021-09-28 20:23:55 +0800

    redirect if name not known

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import LandingPage from 'components/landing-page';
 import ExampleApiCall from 'components/common/example-api-call';
 import UserForm from 'components/user-form';
@@ -14,21 +14,32 @@ function App() {
     <div className='App'>
       <Router>
         <Switch>
-          <Route exact path='/' component={LandingPage} />
-          <Route path='/preferences' component={UserForm} />
-          <Route path='/results' component={Results} />
-          <Route path='/restaurant-details' component={FoodSelectionPage} />
-          <Route path='/create-room' component={UserForm} />
-          <Route path='/example-api-call' component={ExampleApiCall} />
-          <Route
-            path='/room/:roomCode/selection'
-            component={FoodSelectionPage}
-          />
-          <Route path='/room/:roomCode' component={Room} />
+            <Route exact path='/' component={LandingPage} />
+            <Route path='/preferences' component={UserForm} />
+            <Route path='/results' component={Results} />
+            <Route path='/restaurant-details' component={FoodSelectionPage} />
+            <Route path='/create-room' component={UserForm} />
+            <Route path='/example-api-call' component={ExampleApiCall} />
+            <RequireNameRoute path='/room/:roomCode/selection'><FoodSelectionPage /></RequireNameRoute>
+            <RequireNameRoute path='/room/:roomCode'><Room /></RequireNameRoute>
         </Switch>
       </Router>
     </div>
-  );
+  )
+}
+
+const RequireNameRoute = ({children, ...rest}) => {
+    return (
+        <Route
+            {...rest}
+            render={ _ => 
+                window.sessionStorage.getItem('name') ? (
+                    children
+                ) : (
+                    <Redirect to='/' />
+                )
+            } />
+    )
 }
 
 export default App;
