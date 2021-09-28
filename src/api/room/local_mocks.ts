2aea26c76d0d95336538ddcce07eea24e331fb7b
quekyufei <quekyufei@hotmail.com>
2021-09-28 20:51:02 +0800

    hotfix photo urls

--- src/api/room/local_mocks.ts
+++ src/api/room/local_mocks.ts
@@ -118,8 +118,8 @@ const MockFoodList = [
         cuisine_type: [1, 4],
         restrictions:[2, 3],
         photos:[
-            'AJFPAISIOasdASOIDJAKJJDOSAoiasjidjPAOISJsdjJAOIIDJASdhosapA',
-            'AJFPAISIOasdASOIDJAKJJDOSAoiasjidjPAOISJsdjJAOIIDJASdhosapA',
+            'https://danielfooddiary.com/wp-content/uploads/2020/03/twomenbagelhouse1.jpg',
+            'https://www.misstamchiak.com/wp-content/uploads/2019/11/DSCF0483-3-e1573616926486.jpg',
         ],
         reviews:[
             {
@@ -152,8 +152,10 @@ const MockFoodList = [
         cuisine_type: [2],
         restrictions:[1, 3],
         photos:[
-            'AJFPAISIOasdASOIDJAKJJDOSAoiasjidjPAOISJsdjJAOIIDJASdhosapA',
-            'AJFPAISIOasdASOIDJAKJJDOSAoiasjidjPAOISJsdjJAOIIDJASdhosapA',
+            'https://cdn.shortpixel.ai/client/to_avif,q_glossy,ret_img,w_450,h_300/https://danielfooddiary.com/wp-content/uploads/2017/10/manman41.jpg',
+            'https://media-cdn.tripadvisor.com/media/photo-s/0f/1f/3c/ef/photo0jpg.jpg',
+            'https://4.bp.blogspot.com/-pG2QheJoPqI/V_4KOTSNEzI/AAAAAAAANPY/IUhqvk2tHg8YRouAzOmrII0tXF1WdVvawCLcB/s1600/umDSCF9361.JPG',
+            'https://live.staticflickr.com/65535/48290857847_ef5efeecd1_b.jpg'
         ],
         reviews:[
             {
@@ -192,7 +194,7 @@ const MockFoodList = [
         reviews: [
             {
                 rating: 3,
-                comment: "lalalalallalal",
+                comment: 'lalalalallalal',
             },
         ]
     }
