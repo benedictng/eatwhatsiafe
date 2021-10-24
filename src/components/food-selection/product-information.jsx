75ad79ce3a8827542078d920cc05bda14f705290
badgalchenchen <45150718+badgalchenchen@users.noreply.github.com>
2021-10-24 17:20:24 +0800

    my changes

--- src/components/food-selection/product-information.jsx
+++ src/components/food-selection/product-information.jsx
@@ -1,25 +1,20 @@
-const ProductInformation = ({ name, address, mrt }) =>
-// const name = "Man Man Japanese Unagi Restaurant";
-// const hours = "11.30am - 3pm";
-// const price = "$$$";
-// const address = "1 Keong Saik Road";
-// const description = "Freshest unagi in Singapore";
+import locationPresetData from '../../common/constants/location-preset-data';
 
-    (
-        <>
-            <div className="">
-                <h1>{name}</h1>
-                {/* <h3>Opening Hours:</h3>
-        <p>{hours}</p> */}
-                {/* <h3>Price:</h3>
-        <p>{price}</p> */}
-                <h3>Address:</h3>
-                <p>{address}</p>
-                <h3>Nearest MRT:</h3>
-                <p>{mrt}</p>
-                {/* <h3>Description</h3>
-        <p>{description}</p> */}
-            </div>
-        </>
-    );
+const ProductInformation = ({
+    name, address, location, hours,
+}) => (
+    <>
+        <div className="">
+            <h1>{name}</h1>
+            <h3>Opening Hours:</h3>
+            {hours.map((hour) => (
+                <p>{hour}</p>
+            ))}
+            <h3>Address:</h3>
+            <p>{address}</p>
+            <h3>Region:</h3>
+            <p>{locationPresetData.enum2[location]}</p>
+        </div>
+    </>
+);
 export default ProductInformation;
