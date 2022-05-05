f036c90225864d65311d930c1828fa3af7eae544
Howard Liu <howard.liu@estl.sg>
2022-05-05 23:13:13 +0800

    added review modal

--- src/components/food-selection/restaurant-card.jsx
+++ src/components/food-selection/restaurant-card.jsx
@@ -4,6 +4,8 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@mui/material/Container';
 
 // import Carousel from './carousel';
+import { useState } from 'react';
+import { Modal, Box } from '@mui/material';
 import Carousel2 from './carousel2';
 
 import ProductInformation from './product-information';
@@ -14,6 +16,30 @@ import Reviews from './reviews';
 
 function RestaurantCard({ restaurant }) {
     // API to call food list
+
+    const [displayReviewModal, setDisplayReviewModal] = useState(undefined);
+
+    const renderReviewModal = () => {
+        if (!displayReviewModal) return null;
+        return (
+            <Modal
+                open={!!displayReviewModal}
+                onClose={() => { setDisplayReviewModal(undefined) }}
+            >
+                <Box
+                    className="reviewModal"
+                >
+                    <p>
+                        Rating:&nbsp;
+                        {displayReviewModal.rating}
+                    </p>
+                    <p>
+                        {displayReviewModal.comment}
+                    </p>
+                </Box>
+            </Modal>
+        )
+    }
 
     return (
         <>
@@ -33,10 +59,13 @@ function RestaurantCard({ restaurant }) {
                         />
                     </Grid>
                     <Grid container spacing={2}>
-                        <Reviews reviews={restaurant.reviews} />
+                        <Reviews
+                            onClick={(review) => { setDisplayReviewModal(review) }}
+                            reviews={restaurant.reviews}
+                        />
                     </Grid>
                 </Grid>
-
+                {renderReviewModal()}
             </Container>
         </>
     );
