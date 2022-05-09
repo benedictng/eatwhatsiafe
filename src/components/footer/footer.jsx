e264d0e3bd74acfc00841b4fd5f0ef54df06953e
Benedict Ng <benedictngbh@gmail.com>
2022-05-09 22:26:05 +0800

    added padding

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
             position: 'fixed', bottom: 0, left: 0, right: 0, 'background-color': '#9FEADD',
         }}
         >
-            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+            <Box px={5} py={2} sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
                 <h3 sx={{ margin: 'auto' }}>made with love; if you like what we do, buy us a coffee</h3>
             </Box>
         </BottomNavigation>
