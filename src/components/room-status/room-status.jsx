7da1e13cf8cd7802fcd39dd409d800ff1227ef40
Howard Liu <howard.liu@estl.sg>
2022-07-14 21:13:06 +0800

    wip added close room dialog

--- src/components/room-status/room-status.jsx
+++ src/components/room-status/room-status.jsx
@@ -3,6 +3,10 @@ import { useParams, useHistory, useLocation } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
 import RoomAPI from 'api/room'
 // import Button from 'react-bootstrap/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -14,6 +18,7 @@ import Footer from 'components/footer'
 
 const Status = ({ roomData }) => {
     const [error, setError] = useState(false);
+    const [closeRoomPopup, setCloseRoomPopup] = useState(false);
     const { roomCode } = useParams()
     const history = useHistory()
 
@@ -134,8 +139,28 @@ const Status = ({ roomData }) => {
                 }
             }
             >
-                {swipedStatus ? <FloatingOrangeButton sx={{ mx: 'auto' }} onClick={closeRoom} buttonText="Close Room" /> : <SwipingButton />}
+                {swipedStatus ? <FloatingOrangeButton sx={{ mx: 'auto' }} onClick={()=>{setCloseRoomPopup(true)}} buttonText="Close Room" /> : <SwipingButton />}
             </Box>
+            <Dialog
+                open={closeRoomPopup}
+                onClose={()=>{setCloseRoomPopup(false)}}
+            >
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description" fontSize={'28px'}>
+                        To reveal the results, you will be closing voting permanently.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button sx={{
+                        backgroundColor: '#E4664D', fontSize: '16px', color: 'black'
+                    }} onClick={()=>{setCloseRoomPopup(false)}}>no, continue voting</Button>
+                    <Button sx={{
+                        backgroundColor: '#C4DC74', fontSize: '16px', color: 'black'
+                    }} onClick={closeRoom}>
+                        yes, end votes
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <Footer/>
         </div>
     )
