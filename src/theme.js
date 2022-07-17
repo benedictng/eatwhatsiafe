b32eb241b49a3f7a185bab16e9a4f8bb7b5f6926
Yufei <quekyufei@hotmail.com>
2022-07-17 19:34:44 +0800

    font family refactor

--- src/theme.js
+++ src/theme.js
@@ -1,39 +1,50 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
+const fontFamilies = {
+    headings: 'OPTIImprovNewWideNine',
+    text: 'soleil,sans-serif',
+}
+
 const theme = responsiveFontSizes(
     createTheme({
         typography: {
-            fontFamily: 'OPTIImprovNewWideNine',
             h0: {
+                fontFamily: fontFamilies.headings,
                 fontSize: '5rem',
                 lineHeight: 1.5,
             },
             h1: {
+                fontFamily: fontFamilies.headings,
                 fontSize: '1.75rem',
                 lineHeight: 1.4,
             },
             h2: {
+                fontFamily: fontFamilies.headings,
                 fontSize: '1.25rem',
             },
             h3: {
+                fontFamily: fontFamilies.headings,
                 fontSize: '1rem',
                 lineHeight: 1.5,
             },
             p: {
-                fontFamily: 'soleil,sans-serif',
+                fontFamily: fontFamilies.text,
                 fontWeight: 400,
                 fontSize: '1rem',
                 lineHeight: '1.33rem',
             },
             p2: {
-                fontFamily: 'soleil,sans-serif',
+                fontFamily: fontFamilies.text,
                 fontWeight: 400,
                 fontSize: '0.875rem',
             },
             subtitle: {
-                fontFamily: 'soleil,sans-serif',
+                fontFamily: fontFamilies.text,
                 fontSize: '1rem',
                 fontWeight: 700,
+            },
+            button: {
+                fontFamily: fontFamilies.headings,
             },
         },
     }),
