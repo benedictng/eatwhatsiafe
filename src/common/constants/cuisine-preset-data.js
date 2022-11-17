74f9c23a307eda7e6c72ca210dacee74635eec51
badgalchenchen <45150718+badgalchenchen@users.noreply.github.com>
2022-11-17 22:29:51 +0800

    Added new cuisines in enum

--- src/common/constants/cuisine-preset-data.js
+++ src/common/constants/cuisine-preset-data.js
@@ -2,21 +2,32 @@ const cuisinePresetData = {
     enum: {
         Chinese: 1,
         Japanese: 2,
-        Muslim: 3,
+        Malay: 3,
         Italian: 4,
         Thai: 5,
         Western: 6,
         Mediterranean: 7,
+        Indian: 8,
+        Korean: 9,
+        Vietnamese: 10,
+        Mexican: 11,
+        Turkish: 12,
     },
 
     enum2: {
         1: 'Chinese',
         2: 'Japanese',
-        3: 'Muslim',
+        3: 'Malay',
         4: 'Italian',
         5: 'Thai',
         6: 'Western',
         7: 'Mediterranean',
+        8: 'Indian',
+        9: 'Korean',
+        10: 'Vietnamese',
+        11: 'Mexican',
+        12: 'Turkish',
+
     },
 };
 
