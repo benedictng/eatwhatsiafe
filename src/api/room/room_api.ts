7b7ec65245eb5858e1e13f92a26be8f64ee637b5
Yufei <quekyufei@hotmail.com>
2023-01-09 21:33:07 +0800

    fix image padding issue

--- src/api/room/room_api.ts
+++ src/api/room/room_api.ts
@@ -6,34 +6,40 @@ import {
     CloseRoomPayload, CloseRoomResponse, RoomResultsPayload, RoomResultsResponse,
 } from './room_api_interface'
 
-const createRoom = (payload: CreateRoomPayload): Promise<CreateRoomResponse> => {
+const createRoom = async (payload: CreateRoomPayload): Promise<CreateRoomResponse> => {
     const url = 'https://eat-what-ah.herokuapp.com/room/create'
-    return axios.post(url, payload).then((res) => res.data)
+    const res = await axios.post(url, payload)
+    return res.data
 }
 
-const getRoomStatus = (payload: RoomStatusPayload): Promise<RoomStatusResponse> => {
+const getRoomStatus = async (payload: RoomStatusPayload): Promise<RoomStatusResponse> => {
     const url = 'https://eat-what-ah.herokuapp.com/room/status'
-    return axios.post(url, payload).then((res) => res.data)
+    const res = await axios.post(url, payload)
+    return res.data
 }
 
-const submitVote = (payload: SubmitVotePayload): Promise<SubmitVoteResponse> => {
+const submitVote = async (payload: SubmitVotePayload): Promise<SubmitVoteResponse> => {
     const url = 'https://eat-what-ah.herokuapp.com/room/vote'
-    return axios.post(url, payload).then((res) => res.data)
+    const res = await axios.post(url, payload)
+    return res.data
 }
 
-const getFoodList = (payload: FoodListPayload): Promise<FoodListResponse> => {
+const getFoodList = async (payload: FoodListPayload): Promise<FoodListResponse> => {
     const url = 'https://eat-what-ah.herokuapp.com/room/foodlist'
-    return axios.post(url, payload).then((res) => res.data)
+    const res = await axios.post(url, payload)
+    return res.data
 }
 
-const getRoomResults = (payload: RoomResultsPayload): Promise<RoomResultsResponse> => {
+const getRoomResults = async (payload: RoomResultsPayload): Promise<RoomResultsResponse> => {
     const url = 'https://eat-what-ah.herokuapp.com/room/results'
-    return axios.post(url, payload).then((res) => res.data)
+    const res = await axios.post(url, payload)
+    return res.data
 }
 
-const closeRoom = (payload: CloseRoomPayload): Promise<CloseRoomResponse> => {
+const closeRoom = async (payload: CloseRoomPayload): Promise<CloseRoomResponse> => {
     const url = 'https://eat-what-ah.herokuapp.com/room/close'
-    return axios.post(url, payload).then((res) => res.data)
+    const res = await axios.post(url, payload)
+    return res.data
 }
 
 const RoomAPIImpl : IRoomAPI = {
--- src/components/food-selection/carousel.jsx
+++ src/components/food-selection/carousel.jsx
@@ -1,28 +1,52 @@
+import { useEffect, useState } from 'react'
 import Carousel from 'react-material-ui-carousel'
 
 import './food-selection.css'
 
 const FoodImageCarousel = ({ photos }) => {
-    const photoMaps = photos.map((photo, id) => (
-        <div
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                height: '100%',
-                width: '100%',
-                margin: 'auto',
-            }}
-        >
-            <img
-                key={id}
-                src={`data:image/jpeg;base64,${photo}`}
-                className="image"
-                alt="Food Pictures"
-            />
-        </div>
-    ))
+    const [photoMaps, setPhotoMaps] = useState(null)
 
-    return (
+    useEffect(() => {
+        const calculateHeight = (naturalHeight, naturalWidth) => {
+            if (naturalHeight >= naturalWidth) {
+                return 500
+            }
+
+            const ratio = naturalHeight / naturalWidth
+            return ratio * 500
+        }
+
+        async function createPhotoElements() {
+            return Promise.all(photos.map(async (photo, id) => {
+                const img = new Image()
+                img.src = `data:image/jpeg;base64,${photo}`
+                await img.decode()
+                const { naturalHeight } = img
+                const { naturalWidth } = img
+                const height = calculateHeight(naturalHeight, naturalWidth)
+                return (
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            height: `${height}px`,
+                            margin: 'auto',
+                        }}
+                    >
+                        <img
+                            key={id}
+                            src={`data:image/jpeg;base64,${photo}`}
+                            className="image"
+                            alt="Food Pictures"
+                        />
+                    </div>
+                )
+            }))
+        }
+        createPhotoElements().then((elements) => setPhotoMaps(elements))
+    }, [])
+
+    return photoMaps === undefined ? <div>Loading</div> : (
         <Carousel
             className="carou-style"
             autoPlay={0}
@@ -31,7 +55,6 @@ const FoodImageCarousel = ({ photos }) => {
                     opacity: 100,
                 },
             }}
-            height={500}
         >
             {photoMaps}
         </Carousel>
